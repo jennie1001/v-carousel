a435523c2f8b9b694aa785b72d56ae5fdde5921f
Jennie Andersson <jennie.andersson@chas.se>
2015-09-27 17:32:45 +0200

    Implemented bxSlider

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,36 @@
 
 var gulp = require('gulp'),
-	uglify = require ('gulp-uglify'),
-	rename = require("gulp-rename"),
-	jshint = require('gulp-jshint');;
+    uglify = require ('gulp-uglify'),
+    minifyCss = require('gulp-minify-css');
+    rename = require("gulp-rename"),
+    concat = require('gulp-concat'),
+    jshint = require('gulp-jshint');
 
-// Scripts Task
+// Gulp Tasks
 gulp.task('scripts', function () {
-	return gulp.src('js/*.js')
-        .pipe(rename({suffix: '.min'}))
+    return gulp.src('js/*.js')
+        .pipe(concat('script.js'))
         .pipe(uglify())
+        .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('build/js'));
 });
 
 gulp.task('styles', function () {
-	console.log('Runs styles');
+    return gulp.src('css/*.css')
+        .pipe(concat('style.css'))
+        .pipe(minifyCss())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('build/css'));
 });
 
 gulp.task('lint', function() {
-  return gulp.src('js/*.js')
+  return gulp.src('js/script.js')
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
 });
 
 gulp.task('watch', function () {
-	gulp.watch('js/*.js', ['scripts', 'lint']);
+    gulp.watch('js/*.js', ['scripts', 'lint']);
 });
 
 gulp.task('default', ['scripts', 'styles', 'lint', 'watch']);
--- js/script.js
+++ js/script.js
@@ -1,3 +1,62 @@
-console.log('Hello world!!');
+var AftonbladetFeed = {
 
-console.log('Hej hej');
+    init: function () {
+        'use strict';
+        AftonbladetFeed.getFeed({
+            url: 'http://www.aftonbladet.se/rss.xml',
+            id: 'bxslider'
+        });
+    },
+    getFeed: function (params) {
+        'use strict';
+        return $.ajax({
+            type: "GET",
+            url: params.url,
+            dataType: "xml",
+            success: function (xml) {
+                $(xml).find('item').each(function () {
+                    var title = '<h2 class="feed-heading">' + $(this).find('title').text() + '</h2>',
+                        dateArr = $(this).find('pubDate').text().split(" "),
+                        dateFormatted = dateArr[1] + ' ' + dateArr[2] + ' ' + dateArr[4],
+                        pubDate = '<span class="feed-pubDate">' + dateFormatted + '</span>',
+                        description = $(this).find("description").text(),
+                        image = $(description)[0],
+                        text = $(description)[1],
+                        sliderItem = '<a href="' + $(this).find('link').text() + '" class="feed-content"></a>';
+                    sliderItem = $(sliderItem).append($(image));
+                    sliderItem = $(sliderItem).append($(title));
+                    sliderItem = $(sliderItem).append($(pubDate));
+                    sliderItem = $(sliderItem).append($(text));
+                    sliderItem = $('<li></li>').append($(sliderItem));
+                    sliderItem.appendTo('#' + params.id);
+                });
+            }
+        });
+    },
+    startSlider: function () {
+        'use strict';
+        var slider = $('#bxslider').bxSlider({
+            mode: 'vertical',
+            minSlides: 3,
+            slideWidth: 500,
+            slideMargin: 10
+        });
+
+        $('#feed-select-news').on('change', function () {
+            var numberOfNews = $('#feed-select-news').val();
+
+            slider.reloadSlider({
+                mode: 'vertical',
+                minSlides: numberOfNews,
+                slideWidth: 500,
+                slideMargin: 10
+            });
+        });
+    }
+};
+
+(function () {
+    'use strict';
+    AftonbladetFeed.init();
+    $(document).ajaxComplete(AftonbladetFeed.startSlider);
+}());
